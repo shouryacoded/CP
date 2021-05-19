e718e7b231ce3db3621867b5c839e52b59804cb7
shouryacoded <shourya.online23@gmail.com>
2021-05-19 09:12:50 +0530

    Update

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,2 +1,22 @@
-ans *=n;
-        n--;
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  system("clear");
+  int n  =0, t=1 ;
+  cout << "Enter a number n";
+  cin >> n;
+  for(int i =0 ; i<n; i++)
+  {
+      for(int j = n; j > i;j--)
+      {
+          cout << " ";
+      }
+      for(int k = 0; k < 2*i-1;k++)
+      {
+          cout << t++ << " ";
+      }
+          cout << endl;
+  }
+  return 0;
+}
--- test.cpp
+++ test.cpp
@@ -3,8 +3,20 @@ using namespace std;
 int main()
 {
   system("clear");
-  int n  =0 ;
+  int n  =0, t=1 ;
   cout << "Enter a number n";
   cin >> n;
+  for(int i =0 ; i<n; i++)
+  {
+      for(int j = n; j > i;j--)
+      {
+          cout << " ";
+      }
+      for(int k = 0; k < i;k++)
+      {
+          cout << t++;
+      }
+          cout << endl;
+  }
   return 0;
 }

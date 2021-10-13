9d7ae677d5eb9518bc2ee16b56f650c0fbd10f81
shouryacoded <shourya.online23@gmail.com>
2021-10-13 10:27:56 +0530

    maintain

--- test.cpp
+++ test.cpp
@@ -1,29 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
-int solve(int size, int a[],int b[])
+int solve(int num)
 {
-    int target = 0;
-    int step = 0;
-    for(int i = 0; i < size; i++)
-    {
-        if(a[i] == b[i])
-        target = i;
-    }
-    int t1 = a[target];
-    int t2 = b[target];
-    for(int j = 0;j < size;j++)
-    {
-        if(a[j]+b[j] == t1 + t2)
-        step++;
-        else if(a[j] == t1 && b[j] != t2)
-        step++;
-        else if(a[j] != t1 && b[j] != t2)
-        step+=2;
-        else if(a[j] != t1 && b[j] == t2)
-        step++;
-        else break;
-    }
-    return step;
+    if(num > 1)
+        return num * solve(num-1);
+    else
+        return 1;
 }
 int main()
 {
@@ -31,19 +13,9 @@ int main()
     cin >> t;
     while (t--)
     {
-        int n;
-        cin >> n;
-        int a[n];
-        int b[n];
-        for(int i=0;i<n;i++)
-        {
-        cin >> a[i];
-        }
-        for(int i=0;i<n;i++)
-        {
-        cin >> b[i];
-        }
-        cout << solve(n, a, b) << endl;
+        int num;
+        cin >> num;
+        cout << solve(num) << endl;
     }
     return 0;
 }

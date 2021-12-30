8b72bc8c6e5d6cb80ef997715cdd1888fa4b503a
shouryacoded <shourya.online23@gmail.com>
2021-12-30 19:39:14 +0530

    leetcode

--- test.cpp
+++ test.cpp
@@ -1,21 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
-int solve(int num)
+
+int maxArea(vector<int> &height)
 {
-    if(num > 1)
-        return num * solve(num-1);
-    else
-        return 1;
+    int left = 0, right = height.size()-1;
+        int curr_area =0, max_area = 0;
+        while(right-left > 1)
+        {
+            int len = right - left;
+            curr_area = min(height[left],height[right]) * len;
+            if(curr_area >= max_area)
+            {
+                max_area = curr_area;
+            }
+            if(height[left] < height[right])
+                left++;
+            else if (height[left] > height[right])
+                right--;
+            else 
+            {
+                left++; right--;
+            }
+        }
+        return max_area;
 }
+
 int main()
 {
-    int t;
-    cin >> t;
-    while (t--)
+    vector<int> a;
+    for(int i = 0; i < 2;i++)
     {
-        int num;
-        cin >> num;
-        cout << solve(num) << endl;
+        int temp;
+        cin >> temp;
+        a.push_back(temp);
     }
+    int res = maxArea(a);
+    cout << res;
     return 0;
 }

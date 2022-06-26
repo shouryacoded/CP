4384ac1f8ded5d3521d87d3713cff45c981a2e8c
shouryacoded <shourya.online23@gmail.com>
2022-06-26 11:48:11 +0530

    Changes

--- test.cpp
+++ test.cpp
@@ -1,12 +1,28 @@
-#include<iostream>
-#include<stdio.h>
-#include<algorithm>
-#include<filesystem>
-#include<math.h>
-
+#include<bits/stdc++.h>
 using namespace std;
+
+#define ll long long
+#define ld long double
+
+int solve(int points)
+{
+    
+    
+}
+
 
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int points;
+        cin >> points;
+        cout << solve(points) << endl;
+    }
     return 0;
 }

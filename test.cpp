f8e01b3152a64c81411a276b85ed40d16cd83773
shouryacoded <shourya.online23@gmail.com>
2022-06-27 00:40:01 +0530

    Updates

--- test.cpp
+++ test.cpp
@@ -1,44 +1,30 @@
 #include<bits/stdc++.h>
+
 using namespace std;
-
-#define ll long long
-#define ld long double
-
-int solve(int size, vector<int> ar)
-{
-    unordered_set<int> st;
-    for(int i = 0; i < size; i++){
-        int temp =  ar[i];
-        st.insert(temp);
-    }
-    cout << endl << "Size of the set : " << st.size() << endl;
-    if(st.size() == 1 && ar.size() > 1)
-        return 0;
-    if(st.size() == ar.size()) 
-        return ar.size();
-    else
-        return ar.size() - st.size();
-}
-
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
     int t;
     cin >> t;
     while(t--)
     {
-        int size;
+        int num;
+        cin >> num;
         int temp;
-        cin >> size;
+        unordered_set<int> st;
         vector<int> ar;
-        for(int i = 0; i < size; i++){
+        for(int i = 0; i < num; i++)
+        {
             cin >> temp;
             ar.emplace_back(temp);
+            st.insert(temp);
         }
-        cout << solve(size, ar);
+        if(st.size() == 1 && ar.size() > 1)
+            cout << 0 << endl;
+        if(st.size() == ar.size())
+            cout << ar.size() - 1 << endl;
+        if(st.size() != ar.size() && st.size() != 1)
+            cout << ar.size() - st.size() << endl;
     }
     return 0;
 }

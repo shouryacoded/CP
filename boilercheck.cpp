58a7f90eae8bee01166b38506bd7ba60d4a50729
shouryacoded <shourya.online23@gmail.com>
2022-07-19 15:36:03 +0530

    Updated the boilerplate to support Atom

--- boilercheck.cpp
+++ boilercheck.cpp
@@ -14,6 +14,12 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
+
+        #ifndef ONLINE_JUDGE
+            freopen("input.txt", "r", stdin);
+            freopen("output.txt", "w", stdout);
+        #endif
+
     vector<int> v;
     v.push_back()
     int t;
